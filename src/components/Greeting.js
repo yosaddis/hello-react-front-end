d27718d96037df0209c0022d6f1e763bc6d2656b
yosaddis <email.to.yoseph@gmail.com>
2023-10-08 05:56:53 +0300

    add greeting component

---
+++ src/components/Greeting.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMessage } from '../redux/features/greetingSlice';
+
+function Greeting() {
+  const { message } = useSelector((state) => state.greeting);
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(fetchMessage());
+  }, [dispatch]);
+
+  return (
+    <div className="tv-container">
+      console.log(message.greeting);
+      <h1>{message.greeting}</h1>
+    </div>
+  );
+}
+
+export default Greeting;
